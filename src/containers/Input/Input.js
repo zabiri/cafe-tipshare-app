be7a07a055c835f181684dfe7da8cae26930eca6
Zach Biri <zachbiri@gmail.com>
2018-06-21 18:36:11 -0500

    Adding tabs from react-tabs

--- src/containers/Input/Input.js
+++ src/containers/Input/Input.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import './Input.css'
 
 export class Input extends Component {
   render() {
     return (
-      <div>
-        <p>Input</p>
-      </div>
+      <Tabs>
+    <TabList>
+      <Tab>Monday-Wednesday</Tab>
+      <Tab>Thursday-Friday</Tab>
+      <Tab>Saturday-Sunday</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2>Monday through wednesday content</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Thursday friday content</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Saturday sunday content</h2>
+    </TabPanel>
+  </Tabs>
     )
   }
 }
