1ee3580afbfee13ec6014c4ed64b90790e6b3aed
zabiri <zachbiri@gmail.com>
2018-06-23 20:22:01 -0500

    Fixing linter issues

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import "./Output.css";
 const Output = props => {
-  let dayBlock = null;
   let content = null;
   let split = 3;
   let payRate = 0;
@@ -21,48 +20,53 @@ const Output = props => {
           <p>The hourly rate was ${payRate} per hour.</p>
         </Fragment>
       );
-      dayBlock = "Monday-Wednesday";
+
       break;
     case "isThurs":
-      supportTips = props.grossTips*.1
-      split = (props.grossTips-supportTips)/3;
+      supportTips = props.grossTips * 0.1;
+      split = (props.grossTips - supportTips) / 3;
       content = (
         <Fragment>
           <p>
-            All three servers made ${split.toFixed(2)}, and the support staff made ${supportTips.toFixed(2)}.
+            All three servers made ${split.toFixed(2)}, and the support staff
+            made ${supportTips.toFixed(2)}.
           </p>
         </Fragment>
       );
-      dayBlock = "Thursday";
+
       break;
     case "isFri":
-      supportTips = props.grossTips * .1
+      supportTips = props.grossTips * 0.1;
       split = (props.grossTips - supportTips) / 4;
       content = (
         <Fragment>
           <p>
-            All four servers made ${split.toFixed(2)}, and the support staff made ${supportTips.toFixed(2)}.
+            All four servers made ${split.toFixed(2)}, and the support staff
+            made ${supportTips.toFixed(2)}.
           </p>
         </Fragment>
       );
-      dayBlock = "Friday";
+
       break;
     case "isWeekend":
-      let serverNumber = props.patio?5:4
-      supportTips = props.grossTips *.15
-      split = (props.grossTips - supportTips -65)/serverNumber
-      content = <Fragment>
+      let serverNumber = props.patio ? 5 : 4;
+      supportTips = props.grossTips * 0.15;
+      split = (props.grossTips - supportTips - 65) / serverNumber;
+      content = (
+        <Fragment>
           <p>
-            All {props.patio?"five":"four"} servers made ${split.toFixed(2)}, and the support staff made ${supportTips.toFixed(2)}.
+            All {props.patio ? "five" : "four"} servers made ${split.toFixed(2)},
+            and the support staff made ${supportTips.toFixed(2)}.
           </p>
           <p>
-            This includes $25.00 for the silverware roller, and a minimum of $40.00 for the kitchen.
+            This includes $25.00 for the silverware roller, and a minimum of
+            $40.00 for the kitchen.
           </p>
-        </Fragment>;
-      dayBlock = "the weekend";
+        </Fragment>
+      );
+
       break;
-    default:
-      dayBlock = null;
+    default: return null
   }
   return <div className="outputBlock">{content}</div>;
 };
