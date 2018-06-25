e4ac1c3e5939561aa20e5dab2c334e32550faaea
zabiri <zachbiri@gmail.com>
2018-06-25 16:05:47 -0500

    Removing useless nesting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from "react";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 
-import Layout from "./containers/Layout/Layout";
+import Header from "./components/Header/Header";
+import Input from "./containers/Input/Input";
 
-import reducer from "./store/reducer";
 import "./App.css";
 
 class App extends Component {
   render() {
-    const store = createStore(
-      reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
     return (
       <div className="App">
-        <Provider store={store}>
-          <Layout />
-        </Provider>
+        <Header />
+        <Input />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import './index.css';
+
 import App from './App';
+
+import reducer from './store/reducer'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store= createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
