6af7a95f7278e13d5bf8a44afebe003093402323
zabiri <zachbiri@gmail.com>
2018-06-25 15:46:16 -0500

    Commenting code blocks

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,8 +7,9 @@ const initialState = {
   lateHours: 6.5
 };
 // This is a utility function to dry up the code for
-// populating the returns in the reducer functions.  If
-// I were using multiple reducers, this would probably
+// populating the returns in the reducer functions.  
+// this avoids mutating the state, which is a nicht-nicht.
+// If I were using multiple reducers, this would probably
 // be in a seperate .js file, but this is a simple app
 // with just one reducer.  Also, the usefulness of this
 // would be greater if I were using less of a
@@ -20,18 +21,15 @@ const updateObject = (oldObject, updatedProperties) => {
   };
 };
 
+// These are the functions my reducer switch case will
+// call.  Note that the names are corresponding camel
+// case versions of the all-caps snake case of the 
+// action types.
 const changeGross = (state, action) => {
   const updatedState = {
     grossTips: action.gross
   };
   return updateObject(state, updatedState);
-};
-
-const togglePatio = (state, action) => {
-  const updatedState = {
-    isPatio: !state.isPatio
-  }
-  return updateObject(state, updatedState)
 };
 
 const changeHours = (state, action) => {
@@ -42,12 +40,22 @@ const changeHours = (state, action) => {
   return updateObject(state, updatedState)
 };
 
+const togglePatio = (state, action) => {
+  const updatedState = {
+    isPatio: !state.isPatio
+  }
+  return updateObject(state, updatedState)
+};
+
+// This is my actual reducer, which I've refactored to
+// be more streamlined and legible.  Instead of logic
+// in the switch case, each case simply returns a
+// function with state and action passed to it.  
 const reducer = (state = initialState, action) => {
-  //if statements or switch cases that immutably update the state like so:
   switch (action.type) {
-    case actionTypes.TOGGLE_PATIO: return togglePatio(state, action);
     case actionTypes.CHANGE_GROSS: return changeGross(state, action);
     case actionTypes.CHANGE_HOURS: return changeHours(state, action);
+    case actionTypes.TOGGLE_PATIO: return togglePatio(state, action);
     default: return state;
   }
 };
