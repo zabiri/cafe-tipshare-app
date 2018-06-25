ff4db62f6a99c124779370ca5f36e2c72f2dd2f3
zabiri <zachbiri@gmail.com>
2018-06-25 16:19:36 -0500

    Commenting Clockout component, converting css to sass for my sanity

--- src/components/Clockout/Clockout.js
+++ src/components/Clockout/Clockout.js
@@ -1,9 +1,15 @@
 import React from "react";
+// TimePicker is a stopgap for now -- I plan on eventually making
+// my own moment.js based component when I have time (no pun intended).
 import TimePicker from "rc-time-picker";
 import moment from "moment";
+
 import "./Clockout.css";
+
 const Clockout = props => {
   const format = "h:mm a";
+// This default clockout time is 3:30 PM, which is a good starting
+// point for weekday and weekend shifts.
   const defaultTime = moment()
     .hour(15)
     .minute(30);
