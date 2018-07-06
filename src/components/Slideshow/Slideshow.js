b0e5c0ddf7b8ee936e58fc1e67913ec151072f65
zabiri <zachbiri@gmail.com>
2018-07-06 13:37:31 -0700

    class != className duh

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./Slideshow.css";
 const Slideshow = () => {
   return (
-    <div class="cb-slideshow__container">
-      <ul class="cb-slideshow">
-        <li class="cb-slideshow__item">
-          <span class="cb-slideshow__frame" />
+    <div className="cb-slideshow__container">
+      <ul className="cb-slideshow">
+        <li className="cb-slideshow__item">
+          <span className="cb-slideshow__frame" />
         </li>
-        <li class="cb-slideshow__item">
-          <span class="cb-slideshow__frame" />
+        <li className="cb-slideshow__item">
+          <span className="cb-slideshow__frame" />
         </li>
-        <li class="cb-slideshow__item">
-          <span class="cb-slideshow__frame" />
+        <li className="cb-slideshow__item">
+          <span className="cb-slideshow__frame" />
         </li>
-        <li class="cb-slideshow__item">
-          <span class="cb-slideshow__frame" />
+        <li className="cb-slideshow__item">
+          <span className="cb-slideshow__frame" />
         </li>
-        <li class="cb-slideshow__item">
-          <span class="cb-slideshow__frame" />
+        <li className="cb-slideshow__item">
+          <span className="cb-slideshow__frame" />
         </li>
       </ul>
     </div>
